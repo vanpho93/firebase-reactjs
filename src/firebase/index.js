d0afc80a646161b1dff94c07c3e395a92b5d16aa
Admin <vanpho02@gmail.com>
2017-03-30 17:56:31 +0700

    updating

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -10,6 +10,49 @@ const config = {
 firebase.initializeApp(config);
 
 //Saving data for whole app
-firebase.database().ref().set({
-    name: 'Nguyen Van Pho'
-});
+const firebaseRef = firebase.database().ref();
+
+// firebaseRef.set({
+//     username: 'Pho',
+//     notes: { 
+//         id: 1, 
+//         subject: 'Hoc Tap', 
+//         content: 'Nop do an cuoi khoa', 
+//         important: true 
+//     }
+// });
+
+//Updating a child prop
+//Set return a promise, so you can call then and catch method of it
+// firebaseRef.child('username').set('Khoa');
+
+//Updating with updata method
+// firebaseRef.update({ username: 'vanpho93' });
+
+//Update multiref
+// firebaseRef.update({
+//     username: 'PhoNguyen',
+//     'notes/id': 2
+// });
+
+//Muti childs works as well
+// firebaseRef.child('notes').child('important').update({ a: 1 });
+// firebaseRef.child('notes/important').update({ a: 1 });
+
+// firebase.child('...').remove();
+//call update with value set to null is another way to remove
+
+//Fetching all data from firebase
+// firebaseRef.once('value')
+// .then(snapshot => console.log(snapshot.val()));
+
+//Fetching some data
+// firebaseRef.child('notes').once('value')
+// .then(snapshot => console.log(snapshot.key, snapshot.val()));
+
+//Firebase data changed handler, you can set on for root and child
+// firebaseRef.on('value', () => console.log('Data changed'));
+
+//Off the firebase listener by passing the function 
+// firebaseRef.off();
+// firebaseRef.off(listener);
